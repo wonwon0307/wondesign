49fa97756a73ff943fdd0479b04b30eb9fdefdf2
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-08-03 10:31:20 +0900

    export types

--- primitives/headless-ui/src/core/floating/index.ts
+++ primitives/headless-ui/src/core/floating/index.ts
@@ -1,2 +1,8 @@
-export type { ArrowPosition, FloatingOptions, FloatingPosition } from "./types";
+export type {
+  ArrowPosition,
+  FloatingAlign,
+  FloatingOptions,
+  FloatingPlacement,
+  FloatingPosition,
+} from "./types";
 export { useFloating } from "./useFloating";
