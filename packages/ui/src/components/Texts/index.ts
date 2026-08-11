ee4a20d3862e8c49f77083b2b6c194a9bb244f32
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-08-11 18:50:11 +0900

    re-export Badge from ui package

--- packages/ui/src/components/Texts/index.ts
+++ packages/ui/src/components/Texts/index.ts
@@ -1,2 +1,5 @@
 export { Text } from "@wondesign/texts/Text";
 export type { TextProps } from "@wondesign/texts/Text";
+
+export { Badge } from "@wondesign/texts/Badge";
+export type { BadgeProps } from "@wondesign/texts/Badge";
