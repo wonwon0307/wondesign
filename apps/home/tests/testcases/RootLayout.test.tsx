1ef30dce76e7bddc32131791331c3afa26302806
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-08-17 17:20:19 +0900

    minor test update

--- apps/home/tests/testcases/RootLayout.test.tsx
+++ apps/home/tests/testcases/RootLayout.test.tsx
@@ -14,8 +14,7 @@ describe("RootLayout", () => {
     expect(getByRole("banner")).toBeTruthy();
     expect(getByText("WonDesign")).toBeTruthy();
     expect(getByText("Primitives")).toBeTruthy();
-    expect(getByText("Design System")).toBeTruthy();
-    expect(getByText("Colors")).toBeTruthy();
+    expect(getByText("Components")).toBeTruthy();
 
     expect(getByRole("main")).toBeTruthy(); // main
     expect(getByText("Main Content")).toBeTruthy();
