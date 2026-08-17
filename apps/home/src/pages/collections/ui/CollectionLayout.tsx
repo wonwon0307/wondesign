80c2d08c65e4264d05096d6f4252404758d4b65c
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-08-17 17:13:49 +0900

    add Collection layout and index-redirect

---
+++ apps/home/src/pages/collections/ui/CollectionLayout.tsx
@@ -0,0 +1,39 @@
+import {
+  SidebarProvider,
+  Sidebar,
+  SidebarNav,
+  SidebarToggle,
+} from "@wondesign/ui/Sidebar";
+import { getSidebar } from "@wondocs/core/sidebar";
+
+import { DocsSidebarItem } from "@/entities/document";
+import { styles } from "./styles.css";
+
+interface Props {
+  params: Promise<{ collection: string }>;
+  children: React.ReactNode;
+}
+
+export async function CollectionLayout({ params, children }: Readonly<Props>) {
+  const { collection } = await params;
+
+  const sidebarItems = getSidebar(collection);
+
+  return (
+    <SidebarProvider keyboardShortkey="Mod+B" defaultOpen>
+      <div className={styles.container}>
+        <Sidebar>
+          <SidebarNav>
+            {sidebarItems.map((item, idx) => (
+              <DocsSidebarItem key={`${item.type}-${idx}`} item={item} />
+            ))}
+          </SidebarNav>
+        </Sidebar>
+        <div>
+          <SidebarToggle />
+          {children}
+        </div>
+      </div>
+    </SidebarProvider>
+  );
+}
---
+++ apps/home/src/pages/collections/ui/styles.css.ts
@@ -0,0 +1,9 @@
+import { style } from "@vanilla-extract/css";
+
+const container = style({
+  flex: 1,
+  display: "flex",
+  flexDirection: "row",
+});
+
+export const styles = { container };
