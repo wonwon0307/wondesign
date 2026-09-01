2e6dc9ec07790561ccb69ea8ef3b8f14c18db00d
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-09-01 16:28:08 +0900

    Add sticky feature to sidebar

--- apps/home/src/pages/collections/ui/styles.css.ts
+++ apps/home/src/pages/collections/ui/styles.css.ts
@@ -6,4 +6,15 @@ const container = style({
   flexDirection: "row",
 });
 
-export const styles = { container };
+const sidebar = style({
+  position: "sticky",
+  top: "48px",
+  left: 0,
+  height: `calc(100vh - 64px)`,
+});
+
+const contents = style({
+  flex: 1,
+});
+
+export const styles = { container, sidebar, contents };
