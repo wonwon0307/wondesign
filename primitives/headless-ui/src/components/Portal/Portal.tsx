701136b1a9884ef8ee9880bd4a49fd3449139f4c
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-09-05 01:06:20 +0900

    remove Portal export

--- primitives/headless-ui/src/components/Portal/Portal.tsx
+++
@@ -1,5 +0,0 @@
-import { Portal as Component } from "@/core/portal";
-
-export function Portal({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <Component isPortalMode>{children}</Component>;
-}
