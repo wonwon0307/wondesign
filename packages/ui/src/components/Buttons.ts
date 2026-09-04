774f9cc341706f907aa49c9ab72907affac9c4f6
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-09-04 13:03:16 +0900

    switch to flat structure

---
+++ packages/ui/src/components/Buttons.ts
@@ -0,0 +1,15 @@
+// Primitives
+export { Pressable } from "@wondesign/buttons/Pressable";
+export type { PressableProps } from "@wondesign/buttons/Pressable";
+
+// Button (Form Button)
+export { Button } from "@wondesign/buttons/Button";
+export type { ButtonProps } from "@wondesign/buttons/Button";
+
+// Icon Button
+export { IconButton } from "@wondesign/buttons/Icon";
+export type { IconButtonProps } from "@wondesign/buttons/Icon";
+
+// Link
+export { LinkButton } from "@wondesign/buttons/Link";
+export type { LinkButtonProps } from "@wondesign/buttons/Link";
