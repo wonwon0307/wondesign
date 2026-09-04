34f7233bae4f3935eb9831d599215d9cbbe14fd1
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-09-04 22:27:25 +0900

    add data

--- components/links/src/NavList/NavLink.tsx
+++ components/links/src/NavList/NavLink.tsx
@@ -19,6 +19,7 @@ export function NavLink({
         {...rest}
         className={clsx(styles.navLink({ isActive }), className)}
         aria-current={isActive ? "page" : undefined}
+        data-active={isActive ? "true" : "false"}
       >
         {children}
       </Anchor>
