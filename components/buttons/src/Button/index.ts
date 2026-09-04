d1213c9ec8c15d82690643a6a3a1072fa2cfa985
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-09-04 13:20:13 +0900

    remove outdated components

--- components/buttons/src/Button/index.ts
+++ components/buttons/src/Button/index.ts
@@ -1,5 +1,2 @@
 export { Button } from "./Button";
 export type { ButtonProps } from "./Button";
-
-export { FormLink } from "./FormLink";
-export type { FormLinkProps } from "./FormLink";
--- components/buttons/src/Icon/index.ts
+++ components/buttons/src/Icon/index.ts
@@ -1,5 +1,2 @@
 export { IconButton } from "./IconButton";
 export type { IconButtonProps } from "./IconButton";
-
-export { IconLink } from "./IconLink";
-export type { IconLinkProps } from "./IconLink";
