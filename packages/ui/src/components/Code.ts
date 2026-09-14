6ae6eb46504cc41b2fde50dae7e7f567cb2728cb
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-09-15 08:23:58 +0900

    re-export from the ui package

---
+++ packages/ui/src/components/Code.ts
@@ -0,0 +1,11 @@
+export { Pre } from "@wondesign/code/CodeBlock";
+export type { PreProps } from "@wondesign/code/CodeBlock";
+
+export { InlineCode } from "@wondesign/code/InlineCode";
+export type { InlineCodeProps } from "@wondesign/code/InlineCode";
+
+export { KeyboardGroup, Keyboard } from "@wondesign/code/Keyboard";
+export type {
+  KeyboardGroupProps,
+  KeyboardProps,
+} from "@wondesign/code/Keyboard";
