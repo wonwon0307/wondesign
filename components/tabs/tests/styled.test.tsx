8dcdb95a09f18cad248a0df57a86766833d8dff5
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-09-17 18:11:06 +0900

    add testcase

--- components/tabs/tests/styled.test.tsx
+++ components/tabs/tests/styled.test.tsx
@@ -1,6 +1,9 @@
 import { render } from "@testing-library/react";
 
+import { TabsProvider } from "@/headless/Provider";
 import { Tabs, type TabEntry } from "@/styled/Tabs";
+import { TabsList } from "@/styled/List";
+import { Tab } from "@/styled/Tab";
 
 describe("StyledTabs", () => {
   const tabs: TabEntry[] = [
@@ -31,5 +34,20 @@ describe("StyledTabs", () => {
     tabs.forEach((tab) => {
       expect(getByText(tab.name)).toBeTruthy();
     });
+  });
+
+  it("respects children over icon and iconSide", () => {
+    const { getByText, queryByText } = render(
+      <TabsProvider>
+        <TabsList>
+          <Tab tabName="tab" icon="chevron-right" iconSide="left">
+            Custom Children
+          </Tab>
+        </TabsList>
+      </TabsProvider>,
+    );
+
+    expect(getByText("Custom Children")).toBeTruthy();
+    expect(queryByText("tab")).toBeNull();
   });
 });
