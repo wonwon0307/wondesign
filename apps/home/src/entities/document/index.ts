38ebe41dd1dcdcb35f9362284605278503c920b6
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-09-21 19:09:44 +0900

    add barrel export

---
+++ apps/home/src/entities/document/index.ts
@@ -0,0 +1,7 @@
+export { getPageData } from "./api/data";
+
+export { TAB_LABELS, TAB_ORDER } from "./models/page";
+
+export { DocumentLink } from "./ui/DocumentLink";
+
+export type { TabEntry } from "./models/page";
