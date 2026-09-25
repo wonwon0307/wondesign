b547c9d4da2c193f295fa6a35460b5172a98eabe
정승원 <vietman2@jeongseung-won-ui-MacBookPro.local>
2026-09-25 10:42:37 +0900

    merge 2 test files into one

--- apps/home/tests/testcases/root.test.tsx
+++ apps/home/tests/testcases/root.test.tsx
@@ -1,6 +1,7 @@
 import { render } from "@testing-library/react";
 
-import { RootLayout } from "@/pages/root/layout";
+import { RootLayout } from "@/app/RootLayout";
+import { NotFound } from "@/pages/not-found";
 
 describe("RootLayout", () => {
   it("renders header, main and footer correctly", () => {
@@ -13,11 +14,19 @@ describe("RootLayout", () => {
     // header
     expect(getByRole("banner")).toBeTruthy();
     expect(getByText("WonDesign")).toBeTruthy();
-    expect(getByText("Primitives")).toBeTruthy();
+    expect(getByText("Foundations")).toBeTruthy();
     expect(getByText("Components")).toBeTruthy();
 
     expect(getByRole("main")).toBeTruthy(); // main
     expect(getByText("Main Content")).toBeTruthy();
     expect(getByRole("contentinfo")).toBeTruthy(); // footer
+  });
+});
+
+describe("NotFound", () => {
+  it("renders the not-found fallback page correctly", () => {
+    const { getByText } = render(<NotFound />);
+
+    expect(getByText("404 - NotFound")).toBeTruthy();
   });
 });
